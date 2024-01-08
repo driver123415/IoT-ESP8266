9a7cd0af158088f09bad3ec296e083115f42a7e8
Programmer <134813508+driver123415@users.noreply.github.com>
2024-01-08 15:01:03 +0700

    Update Program_Arduino_IDE.ino

--- Program_Arduino_IDE.ino
+++ Program_Arduino_IDE.ino
@@ -16,39 +16,42 @@
 #define OLED_RESET -1                // memberikan nilai variable pada layar oled -1 digunakan karna pin reset tidak digunakan
 #define SCREEN_ADDRESS 0x3C          //memberikan nilai hexadecimal pada layar oled pada bus I2C
 
-
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);   //menggunakan varible display untuk memanggil 4 variable yang sudah dijumlahkan dalam tanda kurung
 
+
+// menggunakan OneWire dan DallasTemperature untuk menghubungkan sensor suhu tipe DS18B20 ke NodeMCU (ESP8266) melalui jalur satu kabel (OneWire)
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature DS18B20(&oneWire);
 
+// menyimpan informasi suhu yang diukur oleh sensor suhu DS18B20
 String tempString, tempString1;
 float temp;
 float Fahrenheit = 0;
 
-char ssid[] = "Nama_Wifi";          //username wifi yang akan digunakan untuk terhubung ke internet
-char password[] = "Password_Wifi";     //password wifi yang digunakan
+char ssid[] = "CREW_SQUAD";          //username wifi yang akan digunakan untuk terhubung ke internet
+char password[] = "opopassword";     //password wifi yang digunakan
 
-#define BOTtoken "0123456789:AAHLSMQv4cJUXtxH35UVbqFiGACSkTNYf4s" //id bot token yang didapat setelah membuat bot pada telegram
+#define BOTtoken "6919629877:AAHLSMQv4cJUXtxH35UVbqFiGACSkTNYf4s"  //id bot token yang didapat setelah membuat bot pada telegram
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 
 // Array untuk menyimpan ID Chat yang diizinkan
-String allowedChats[] = {"0123456789", "your_second_id", "your_third_id"}; //id telegram orang yang menggunakan telegram setelah mendapat dari user info bot
+String allowedChats[] = {"1762649368", "your_second_id", "your_third_id"};  //id telegram orang yang menggunakan telegram setelah mendapat dari user info bot
 
 unsigned long lastTemperatureSendTime = 0;
 const unsigned long temperatureSendInterval = 120000; // setiap 2 menit mengirim pesan suhu ke telegram
-int Bot_mtbs = 1000;
+
+int Bot_mtbs = 1000;  //digunakan untuk mengatur frekuensi pemanggilan fungsi getUpdates dari objek bot
 long Bot_lasttime;
 
 void handleNewMessages(int numNewMessages) {
-  Serial.println("handleNewMessages");
+  Serial.println("handleNewMessages");                    //menampilkan serial monitor
   Serial.println(String(numNewMessages));
 
+  //digunakan untuk menangani pesan-pesan baru yang diterima dari Telegram. Mari kita bahas setiap barisnya
   for (int i = 0; i < numNewMessages; i++) {
     String chat_id = String(bot.messages[i].chat_id);
     String text = bot.messages[i].text;
-
     String from_name = bot.messages[i].from_name;
     if (from_name == "") from_name = "Guest";
 
@@ -65,20 +68,20 @@ void handleNewMessages(int numNewMessages) {
         suhu += " °F";
         bot.sendMessage(chat_id, suhu, "");
       } 
-      else if (text == "/kipas1on") {
-        digitalWrite(kipas1, LOW);
+      else if (text == "/kipas1on") {                         //text pada telegram yang digunakan sebagai pengontrol
+        digitalWrite(kipas1, LOW);                            //memanggil variable pin kipas 1 untuk LOW
         bot.sendMessage(chat_id, "Kipas 1 sudah menyala", "");
       } 
-      else if (text == "/kipas1off") {
-        digitalWrite(kipas1, HIGH);                              //memanggil variable pin kipas 1 untuk high
-        bot.sendMessage(chat_id, "Kipas 1 sudah mati", "");      //tulisan yang muncul pada telegram setelah kipas sudah mati
-      } 
-      else if (text == "/start") {                               //variable yang muncul setelah mengetik kata start pada telegram  
-        String welcome = "Welcome " + from_name + " .\n";        // nama tulisan yang muncul 
-        welcome += "/ceksuhu : Cek suhu ruangan\n";              // tulisan yang muncul pada telegram  
-        welcome += "/kipas1on : Nyalakan kipas1\n";              // tulisan yang muncul pada telegram       
-        welcome += "/kipas1off : Matikan kipas1\n";              // tulisan yang muncul pada telegram   
-        bot.sendMessage(chat_id, welcome, "Markdown");           //mengirim ke telegram
+      else if (text == "/kipas1off") {                        //text pada telegram yang digunakan sebagai pengontrol
+        digitalWrite(kipas1, HIGH);                           //memanggil variable pin kipas 1 untuk HIGH
+        bot.sendMessage(chat_id, "Kipas 1 sudah mati", "");   //tulisan yang muncul pada telegram setelah kipas sudah mati
+      }  
+      else if (text == "/start") {                            //variable yang muncul setelah mengetik kata start pada telegram 
+        String welcome = "Welcome " + from_name + " .\n";     // nama tulisan yang muncul 
+        welcome += "/ceksuhu : Cek suhu ruangan\n";           // tulisan yang muncul pada telegram 
+        welcome += "/kipas1on : Nyalakan kipas1\n";           // tulisan yang muncul pada telegram 
+        welcome += "/kipas1off : Matikan kipas1\n";           // tulisan yang muncul pada telegram 
+        bot.sendMessage(chat_id, welcome, "Markdown");        //mengirim ke telegram
       }
     } else {
       // ID Chat tidak valid, kirim pesan ke pengirim
@@ -88,8 +91,8 @@ void handleNewMessages(int numNewMessages) {
   }
 }
 
-void sendTemperatureToTelegram() {
-  // Pemeriksaan ID Chat sebelum mengirim suhu
+void sendTemperatureToTelegram() { 
+  // Pemeriksaan ID Chat sebelum mengirim dan suhu data suhu dari sensor DS18B20 dan mengirimkannya ke pengguna melalui layanan Telegram
   for (int i = 0; i < sizeof(allowedChats) / sizeof(allowedChats[0]); i++) {
     String chat_id = allowedChats[i];
     if (chat_id != "") {
@@ -109,106 +112,120 @@ void sendTemperatureToTelegram() {
 }
 
 void setup() {
-  pinMode(kipas1, OUTPUT);    // variale kipas1 dipanggil dan digunakan sebagai output
-  digitalWrite(kipas1, HIGH); // kondisi pin diawal
-  pinMode(kipas2, OUTPUT);    // variale kipas2 dipanggil dan digunakan sebagai output
-  digitalWrite(kipas2, LOW);  // kondisi pin diawal 
-  Serial.begin(115200);
+  pinMode(kipas1, OUTPUT);     // variale kipas1 dipanggil dan digunakan sebagai output
+  digitalWrite(kipas1, HIGH);  // kondisi pin diawal 
+  pinMode(kipas2, OUTPUT);     // variale kipas2 dipanggil dan digunakan sebagai output
+  digitalWrite(kipas2, LOW);   // kondisi pin diawal 
+  Serial.begin(115200);        // menginisialisasi komunikasi serial pada perangkat ESP8266 dengan kecepatan baud 11520
 
+
+  // menginisialisasi koneksi WiFi dan komponen lainnya
   DS18B20.begin();
   Wire.begin();
   client.setInsecure();
   WiFi.mode(WIFI_STA);
   delay(100);
-  Serial.print("Connecting Wifi: ");
+  Serial.print("Connecting Wifi: ");    //menampilkan pada serial monitor saat terkoneksi dengan wifi
   Serial.println(ssid);
   WiFi.begin(ssid, password);
 
+  //menunggu hingga perangkat ESP8266 berhasil terhubung ke jaringan Wi-Fi sebelum melanjutkan eksekusi program
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
-
+  
+  //menginisialisasi layar OLED menggunakan perpustakaan Adafruit_SSD1306
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     while (true) {
       delay(1000);
     }
   }
-
+  
+  // mengatur tampilan pada layar OLED setelah perangkat berhasil terhubung ke jaringan Wi-Fi
   delay(2000);
   display.clearDisplay();
-
   display.setTextSize(1);
   display.setTextColor(WHITE);
-
   displayDisplayCenter("Teknik Elektro", 4);
   displayDisplayCenter("POLINEMA 2024", 56);
-
   display.setTextSize(2);
   display.setCursor(0, 32);
-
   tempString.reserve(10);
 }
 
 void loop() {
+  //membaca suhu dari sensor DS18B20, mengonversinya ke Celsius dan Fahrenheit, dan kemudian menampilkannya di Serial Monitor.
   DS18B20.requestTemperatures();
   temp = DS18B20.getTempCByIndex(0);
   Fahrenheit = DS18B20.toFahrenheit(temp);
   Serial.println(temp);
   Serial.println(Fahrenheit);
 
+
+  // mengonversi nilai suhu (temp) menjadi string yang akan ditampilkan di layar OLED
   tempString = String(temp, 2);
   tempString += (char)247;
   tempString += "C";
 
+
+  //mengubah tampilan di layar OLED jika nilai tempString berbeda dengan nilai sebelumnya (tempString1).
   if (tempString != tempString1) {
-    display.setTextColor(BLACK);
+    display.setTextColor(BLACK);  //Menampilkan text color hitam
     displayDisplayCenter(tempString1, 28);
   }
 
-  tempString1 = tempString;
-  Serial.println(tempString);
-
-  display.setTextColor(WHITE);
+ 
+  tempString1 = tempString;    // memperbarui nilai tempString1 dengan nilai tempString saat ini 
+  Serial.println(tempString);  // mencetak nilai tempString ke Serial Monitor
+  display.setTextColor(WHITE); // menampilkan nilai tempString di layar OLED dengan warna teks putih
   displayDisplayCenter(tempString, 28);
 
+
+  //Potongan kode ini memeriksa apakah sudah waktunya mengirim data suhu ke Telegram
   if (millis() - lastTemperatureSendTime >= temperatureSendInterval) {
-    lastTemperatureSendTime = millis();   // Perbarui waktu terakhir pengiriman
+    lastTemperatureSendTime = millis();
     sendTemperatureToTelegram();
   }
 
+  //perintah dimana jika suhu 35 derajat maka kipas 2 akan HIGH dan akan mengirimkan pesan
   if (temp > 35.00) {
-    digitalWrite(kipas2, HIGH);
+    digitalWrite(kipas2, HIGH);               //jika suhu 35 derajat maka kipas2 akan HIGH
     for (int i = 0; i < sizeof(allowedChats) / sizeof(allowedChats[0]); i++) {
       String chat_id = allowedChats[i];
       if (chat_id != "") {
-        bot.sendMessage(chat_id, "Kipas 2 aktif.", "");
+        bot.sendMessage(chat_id, "Kipas 2 aktif.", ""); //mengirim pesan ke telegram
       }
     }
-  } else if (temp < 30.00) {
-    digitalWrite(kipas2, LOW);
+  } 
+  else if (temp < 30.00) {
+    digitalWrite(kipas2, LOW); //Jika suhu sudah menyentuh dibawah 30 derajat maka kipas akan LOW
   }
 
-  if (temp > 35.00) {
-    bot.sendChatAction(allowedChats[0], "Sedang mengetik...");
-    Serial.print("Suhu saat ini : ");
+
+  if (temp > 35.00) {                                           // ketika suhu (temp) melebihi 35.00 derajat Celsius
+    bot.sendChatAction(allowedChats[0], "Sedang mengetik...");  //indikasi bahwa bot sedang memproses pesan atau tindakan
+    Serial.print("Suhu saat ini : ");                           //menampilkan pada serial monitor
     Serial.println(temp);
     delay(2000);
 
-    String suhu = "Intensitas suhu : ";
-    suhu += String(temp, 2);
-    suhu += " *C\n";
-    suhu += "Suhu maksimal\n";
-    for (int i = 0; i < sizeof(allowedChats) / sizeof(allowedChats[0]); i++) {
-      String chat_id = allowedChats[i];
-      if (chat_id != "") {
-        bot.sendMessage(chat_id, suhu, "");
+    String suhu = "Intensitas suhu : ";   //inisialisasi string suhu dengan teks awal "Intensitas suhu
+    suhu += String(temp, 2);              // Menambahkan nilai suhu (temp) ke dalam string suhu dengan dua digit di belakang koma
+    suhu += " *C\n";                      //Menambahkan teks " *C" (mengindikasikan satuan Celsius) dan karakter newline ("\n") ke string suhu
+    suhu += "Suhu maksimal\n";            //Menambahkan teks "Suhu maksimal" dan karakter newline ("\n") ke string suhu
+    
+    for (int i = 0; i < sizeof(allowedChats) / sizeof(allowedChats[0]); i++) {  // Loop untuk mengirim pesan ke setiap ID chat yang diizinkan
+      String chat_id = allowedChats[i];                                         //Mengambil ID chat dari array allowedChats
+      if (chat_id != "") {                                                      //Memeriksa apakah ID chat tidak kosong
+        bot.sendMessage(chat_id, suhu, "");                                     //Mengirim pesan ke ID chat dengan isi pesan suhu
       }
     }
-    Serial.print("Mengirim data sensor ke telegram");
+    Serial.print("Mengirim data sensor ke telegram");                           //menampilkan pada serial monitor
   }
 
+
+  //memeriksa pesan-pesan baru dari bot dan menjalankan fungsi handleNewMessages untuk memproses pesan-pesan
   if (millis() > Bot_lasttime + Bot_mtbs) {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
@@ -221,18 +238,19 @@ void loop() {
   }
 }
 
-void displayDisplayCenter(String text, int posisi) {
-  int16_t x1, y1;
-  uint16_t width, height;
+void displayDisplayCenter(String text, int posisi) {            //menampilkan teks secara pusat pada layar OLED
+  int16_t x1, y1;             //Mendeklarasikan dua variabel bertipe data int16_t (x1 dan y1) yang akan digunakan untuk menyimpan koordinat titik paling kiri atas dari teks
+  uint16_t width, height;     // Mendeklarasikan dua variabel bertipe data uint16_t (width dan height) yang akan digunakan untuk menyimpan lebar dan tinggi kotak pembatas teks
 
-  display.getTextBounds(text, 0, 0, &x1, &y1, &width, &height);
+  display.getTextBounds(text, 0, 0, &x1, &y1, &width, &height);  // untuk mendapatkan batas teks yang akan ditampilkan
 
-  display.setCursor((SCREEN_WIDTH - width) / 2, posisi);
-  display.println(text);
-  display.display();
+  display.setCursor((SCREEN_WIDTH - width) / 2, posisi);  //Menetapkan posisi kursor untuk menampilkan tek
+  display.println(text);                                  // Menampilkan teks dengan menggunakan metode println dari objek display
+  display.display();                                      //Memperbarui tampilan layar OLED agar perubahan yang dilakukan terlihat
 }
 
 bool isAllowedChat(String chatId) {
+  //memeriksa apakah suatu ID chat tertentu termasuk dalam daftar ID chat yang diizinkan (allowedChats)
   // Mengecek apakah chatId ada dalam array allowedChats
   for (int i = 0; i < sizeof(allowedChats) / sizeof(allowedChats[0]); i++) {
     if (allowedChats[i] == chatId) {
